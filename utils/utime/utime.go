9a902b1d8e5e444d3c457f40d63701915b173f9c
Ustyantsev Makar <ustyantsev.makar1@gmail.com>
2025-01-19 09:03:14 +0300

    format and ci

--- utils/utime/utime.go
+++ utils/utime/utime.go
@@ -6,26 +6,23 @@ import "time"
 // is a 60 bit timestamp from 15 Oct 1582 UTC
 // with 100 ns intervals
 
-
 // the problem with the "time" package in golang is
 // the timestamp nanoseconds are stored as a int64
 // so you cannot directly sub from now to 1582
 // you have to first sub unix, then greg
 
-
-
 func GetUUIDTime() uint64 {
-    var timestamp uint64
+	var timestamp uint64
 	now := time.Now().UTC()
-    greg := time.Date(1582, time.October, 15, 0, 0, 0, 0, time.UTC)
+	greg := time.Date(1582, time.October, 15, 0, 0, 0, 0, time.UTC)
 
-    unix := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
+	unix := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-    gregToUnix := unix.Sub(greg).Nanoseconds()
+	gregToUnix := unix.Sub(greg).Nanoseconds()
 
-    timestamp += uint64(gregToUnix)
+	timestamp += uint64(gregToUnix)
 
-    timestamp += uint64(now.Sub(unix).Nanoseconds())
+	timestamp += uint64(now.Sub(unix).Nanoseconds())
 
 	return timestamp
 }
